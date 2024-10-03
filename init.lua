5e141d90145b7f964a71d2db7a3a0b7c1bcbba62
Kuvam Bhardwaj <kuvambhardwaj0529@gmail.com>
2024-10-03 22:04:50 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -736,6 +736,8 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+
+        sql = { 'sql_formatter' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
